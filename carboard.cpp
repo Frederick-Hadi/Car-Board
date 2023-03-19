c275aacf6a72a77bbde17b14f9ce7986eb89e96a
Frederick Hadi <gangstapotato36@gmail.com>
2023-03-19 16:46:22 +1100

    main menu loop moved to carboard

--- carboard.cpp
+++ carboard.cpp
@@ -13,8 +13,17 @@ using std::endl;
 #define STUDENT_ID "s3953344"
 #define STUDENT_EMAIL "s3953344@student.rmit.edu.au"
 
+#define PLAY_COMMAND "1"
+#define SHOW_INFO_COMMAND "2"
+#define QUIT_GAME "3"
 
-
+void showStudentInformation(std::string name, std::string id, std::string email){
+    Helper::printSeparator();
+    std::cout << "Name: " << name << std::endl;
+    std::cout << "No: " << id << std::endl;
+    std::cout << "Email: " << email << std::endl;
+    Helper::printSeparator();
+}
 
 
 
@@ -24,12 +33,30 @@ int main()
      * TODO: here's the main function. You can write the "main menu" loop/code
      * here or you can make separate functions - up to you.
      */
-    Game* game = new Game();
-    game->start();
+    // Game* game = new Game();
+    // game->start();
 
+    Helper::showMainMenu();
+    std::cout << std::endl << "Please enter your choice: ";
+    std::string userInput = Helper::readInput();
+
+    while (userInput != QUIT_GAME && !std::cin.eof()) {
+        if (userInput == PLAY_COMMAND) {
+            Game* game = new Game();
+            game->start();
+            delete game;
+        } else if (userInput == SHOW_INFO_COMMAND) {
+            showStudentInformation(STUDENT_NAME, STUDENT_ID, STUDENT_EMAIL);
+        } else {
+            Helper::printInvalidInput();
+        }
+
+        Helper::showMainMenu();
+        std::cout << std::endl << "Please enter your choice: ";
+        userInput = Helper::readInput();
+    }
 
     std::cout << "Good bye!\n\n";
-    delete game;
     return EXIT_SUCCESS;
 }
 
--- game.cpp
+++ game.cpp
@@ -5,17 +5,8 @@
 #define STUDENT_ID "s3953344"
 #define STUDENT_EMAIL "s3953344@student.rmit.edu.au"
 
-#define PLAY_COMMAND "1"
-#define SHOW_INFO_COMMAND "2"
-#define QUIT_GAME "3"
 
-void showStudentInformation(std::string name, std::string id, std::string email){
-    Helper::printSeparator();
-    std::cout << "Name: " << name << std::endl;
-    std::cout << "No: " << id << std::endl;
-    std::cout << "Email: " << email << std::endl;
-    Helper::printSeparator();
-}
+
 
 /*
  * Show board loading options
@@ -46,56 +37,23 @@ Game::~Game()
 
 void Game::start()
 {
-    //TODO
+    board = new Board();
+    std::string userInput = Helper::readInput();
+
     
-    Helper::showMainMenu();
-    std::cout << std::endl << "Please enter your choice: ";
-    
-    // read user input
-    std::string userInput;
-    userInput = Helper::readInput();
-
-    //TODO: REPLACE "3" AND STUFF WITH #DEFINES
-    // quit once user inputs 3
-    while (userInput != QUIT_GAME && !std::cin.eof()) {
-        if (userInput == SHOW_INFO_COMMAND) {
-            showStudentInformation(STUDENT_NAME, STUDENT_ID, STUDENT_EMAIL);
-        }
-        else if (userInput == PLAY_COMMAND) {
-            board = new Board();
-            Helper::showPlayMenu();
-            board->display(player);
-            
-            bool boardLoaded = false;
-            while (userInput != COMMAND_QUIT) {
-                showOptions(boardLoaded);
-                boardLoaded = Game::loadBoard();
-
-                if (boardLoaded) {
-                    showOptions(boardLoaded);
-                    std::cout << std::endl << "Please enter command: ";
-                    userInput = Helper::readInput();
-                    std::vector<std::string> command;
-                    Helper::splitString(userInput, command, " ");
-                    
-
-                } else {
-                    Helper::printInvalidInput();
-                }
-                
-                
-                std::cout << std::endl << "Please enter command: ";
-                userInput = Helper::readInput();
-            }
-        }
-        else {
+    bool boardLoaded = false;
+    while (userInput != COMMAND_QUIT) {
+        if (boardLoaded) {
+            // fill in
+        } else {
             Helper::printInvalidInput();
         }
-
-        // main start menu asking for input again
-        std::cout << std::endl << "Please enter your choice: ";
+        
+        
+        std::cout << std::endl << "Please enter command: ";
         userInput = Helper::readInput();
     }
+            
     play();
 }
 
